6cb2289f98002e0eabc3f84774cecc3458d9811b
Eddy Lin <elindy26@gmail.com>
2021-08-02 11:23:59 -0400

    Added sync subcommand stub

---
+++ releaser/cmd/sync.go
@@ -0,0 +1,51 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// syncCmd represents the sync command
+var syncCmd = &cobra.Command{
+	Use:   "sync",
+	Short: "Syncs the versions of a repo's dependencies",
+	Long: `Updates version numbers of :
+- Checks that Git tags do not already exist for the new module set version.
+- Checks that the working tree is clean.
+- Switches to a new branch called pre_release_<module set name>_<new version>.
+- Updates module versions in all go.mod files.
+- 'make lint' and 'make ci' are called
+- Adds and commits changes to Git`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("sync called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(syncCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// syncCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// syncCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
