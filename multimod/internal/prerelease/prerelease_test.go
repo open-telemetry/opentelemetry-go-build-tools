00b8a1fd3de1433b4732f75d22c87dbed02fd4e6
Anthony J Mirabella <a9@aneurysm9.com>
2021-08-31 14:29:08 -0400

    Ensure multimod prerelaease can rewrite versions for indirect module deps
    Signed-off-by: Anthony J Mirabella <a9@aneurysm9.com>

---
+++ multimod/internal/prerelease/prerelease_test.go
@@ -0,0 +1,75 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package prerelease
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestReplaceModVersion(t *testing.T) {
+	for _, s := range []struct {
+		name     string
+		input    []byte
+		expected []byte
+		err      bool
+	}{
+		{
+			name: "simple",
+			input: []byte(`module test
+go 1.17
+
+require (
+	foo.bar/baz v1.2.3
+)
+`),
+			expected: []byte(`module test
+go 1.17
+
+require (
+	foo.bar/baz v1.2.4
+)
+`),
+		},
+		{
+			name: "indirect",
+			input: []byte(`module test
+go 1.17
+
+require (
+	foo.bar/baz v1.2.3 // indirect
+)
+`),
+			expected: []byte(`module test
+go 1.17
+
+require (
+	foo.bar/baz v1.2.4 // indirect
+)
+`),
+		},
+	} {
+		t.Run(s.name, func(t *testing.T) {
+			got, err := replaceModVersion("foo.bar/baz", "v1.2.4", s.input)
+			assert.Equal(t, string(s.expected), string(got))
+			if s.err {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
