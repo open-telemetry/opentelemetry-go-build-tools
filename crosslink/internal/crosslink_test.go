88e972c3ce58a67c60e818f8fcedde6f3cc2626c
Bryan Aguilar <bryaag@amazon.com>
2022-02-01 10:54:23 -0800

    Refactor exclude test to use subtest

--- crosslink/internal/crosslink_test.go
+++ crosslink/internal/crosslink_test.go
@@ -370,68 +370,71 @@ func TestExclude(t *testing.T) {
 	}
 
 	for _, tc := range tests {
-		tmpRootDir, err := os.MkdirTemp(testDataDir, testName)
-		if err != nil {
-			t.Fatal("creating temp dir:", err)
-		}
-
-		mockDataDir := filepath.Join(mockDataDir, testName)
-		cp.Copy(mockDataDir, tmpRootDir)
-
-		assert.NotPanics(t, func() { Crosslink(tc.config) })
-		if assert.NoError(t, err, "error message on execution %s") {
-			// a mock_test_data_expected folder could be built instead of building expected files by hand.
-			modFilesExpected := map[string][]byte{
-				filepath.Join(tmpRootDir, "go.mod"): []byte("module go.opentelemetry.io/build-tools/crosslink/testroot\n\n" +
-					"go 1.17\n\n" +
-					"require (\n\t" +
-					"go.opentelemetry.io/build-tools/crosslink/testroot/testA v1.0.0\n" +
-					")\n" +
-					"replace go.opentelemetry.io/build-tools/crosslink/testroot/testA => ../testA\n\n" +
-					"replace go.opentelemetry.io/build-tools/excludeme => ../excludeme\n\n"),
-				filepath.Join(tmpRootDir, "testA", "go.mod"): []byte("module go.opentelemetry.io/build-tools/crosslink/testroot/testA\n\n" +
-					"go 1.17\n\n" +
-					"require (\n\t" +
-					"go.opentelemetry.io/build-tools/crosslink/testroot/testB v1.0.0\n" +
-					")\n" +
-					"replace go.opentelemetry.io/build-tools/crosslink/testroot/testB => ../testB"),
-				filepath.Join(tmpRootDir, "testB", "go.mod"): []byte("module go.opentelemetry.io/build-tools/crosslink/testroot/testB\n\n" +
-					"go 1.17\n\n"),
+		t.Run(tc.testCase, func(t *testing.T) {
+			tmpRootDir, err := os.MkdirTemp(testDataDir, testName)
+			if err != nil {
+				t.Fatal("creating temp dir:", err)
 			}
 
-			for modFilePath, modFilesExpected := range modFilesExpected {
-				modFileActual, err := os.ReadFile(modFilePath)
+			mockDataDir := filepath.Join(mockDataDir, testName)
+			cp.Copy(mockDataDir, tmpRootDir)
 
-				if err != nil {
-					t.Fatalf("TestCase: %s, error reading actual mod files: %v", tc.testCase, err)
+			assert.NotPanics(t, func() { Crosslink(tc.config) })
+			if assert.NoError(t, err, "error message on execution %s") {
+				// a mock_test_data_expected folder could be built instead of building expected files by hand.
+				modFilesExpected := map[string][]byte{
+					filepath.Join(tmpRootDir, "go.mod"): []byte("module go.opentelemetry.io/build-tools/crosslink/testroot\n\n" +
+						"go 1.17\n\n" +
+						"require (\n\t" +
+						"go.opentelemetry.io/build-tools/crosslink/testroot/testA v1.0.0\n" +
+						")\n" +
+						"replace go.opentelemetry.io/build-tools/crosslink/testroot/testA => ../testA\n\n" +
+						"replace go.opentelemetry.io/build-tools/excludeme => ../excludeme\n\n"),
+					filepath.Join(tmpRootDir, "testA", "go.mod"): []byte("module go.opentelemetry.io/build-tools/crosslink/testroot/testA\n\n" +
+						"go 1.17\n\n" +
+						"require (\n\t" +
+						"go.opentelemetry.io/build-tools/crosslink/testroot/testB v1.0.0\n" +
+						")\n" +
+						"replace go.opentelemetry.io/build-tools/crosslink/testroot/testB => ../testB"),
+					filepath.Join(tmpRootDir, "testB", "go.mod"): []byte("module go.opentelemetry.io/build-tools/crosslink/testroot/testB\n\n" +
+						"go 1.17\n\n"),
 				}
 
-				actual, err := modfile.Parse("go.mod", modFileActual, nil)
-				if err != nil {
-					t.Fatalf("error decoding original mod files: %v", err)
-				}
-				actual.Cleanup()
+				for modFilePath, modFilesExpected := range modFilesExpected {
+					modFileActual, err := os.ReadFile(modFilePath)
 
-				expected, err := modfile.Parse("go.mod", modFilesExpected, nil)
-				if err != nil {
-					t.Fatalf("TestCase: %s ,error decoding expected mod file: %v", tc.testCase, err)
-				}
-				expected.Cleanup()
+					if err != nil {
+						t.Fatalf("TestCase: %s, error reading actual mod files: %v", tc.testCase, err)
+					}
 
-				// replace structs need to be assorted to avoid flaky fails in test
-				replaceSortFunc := func(x, y *modfile.Replace) bool {
-					return x.Old.Path < y.Old.Path
-				}
+					actual, err := modfile.Parse("go.mod", modFileActual, nil)
+					if err != nil {
+						t.Fatalf("error decoding original mod files: %v", err)
+					}
+					actual.Cleanup()
 
-				if diff := cmp.Diff(expected, actual, cmpopts.IgnoreFields(modfile.Replace{}, "Syntax"),
-					cmpopts.IgnoreFields(modfile.File{}, "Require", "Exclude", "Retract", "Syntax"),
-					cmpopts.SortSlices(replaceSortFunc),
-				); diff != "" {
-					t.Errorf("TestCase: %s \n Replace{} mismatch (-want +got):\n%s", tc.testCase, diff)
+					expected, err := modfile.Parse("go.mod", modFilesExpected, nil)
+					if err != nil {
+						t.Fatalf("TestCase: %s ,error decoding expected mod file: %v", tc.testCase, err)
+					}
+					expected.Cleanup()
+
+					// replace structs need to be assorted to avoid flaky fails in test
+					replaceSortFunc := func(x, y *modfile.Replace) bool {
+						return x.Old.Path < y.Old.Path
+					}
+
+					if diff := cmp.Diff(expected, actual, cmpopts.IgnoreFields(modfile.Replace{}, "Syntax"),
+						cmpopts.IgnoreFields(modfile.File{}, "Require", "Exclude", "Retract", "Syntax"),
+						cmpopts.SortSlices(replaceSortFunc),
+					); diff != "" {
+						t.Errorf("TestCase: %s \n Replace{} mismatch (-want +got):\n%s", tc.testCase, diff)
+					}
 				}
 			}
-		}
-		os.RemoveAll(tmpRootDir)
+			os.RemoveAll(tmpRootDir)
+		})
+
 	}
 }
 
