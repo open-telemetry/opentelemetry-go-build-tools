d7770591918891b182dad972bf717103590cd2bd
bryan-aguilar <46550959+bryan-aguilar@users.noreply.github.com>
2022-02-18 13:40:33 -0800

    Update crosslink/cmd/prune.go
    Co-authored-by: Tyler Yahn <MrAlias@users.noreply.github.com>

--- crosslink/cmd/prune.go
+++ crosslink/cmd/prune.go
@@ -23,7 +23,7 @@ import (
 var pruneCmd = &cobra.Command{
 	Use:   "prune",
 	Short: "Remove unnecessary replace statements from intra-repository go.mod files",
-	Long: `Prune will analyze and remove any uncessary replace statements that are still
+	Long: `Prune will analyze and remove any unnecessary replace statements that are still
 	included in the intra-repository go.mod files. This is a destructive action and will overwrite
 	existing go.mod files. Prune will not remove modules that fall outside of the root
 	module namespace.`,
