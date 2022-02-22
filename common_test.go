34750d8d6ca6f30e8c33f4b95c5eda49911c7b37
Tyler Yahn <codingalias@gmail.com>
2022-02-22 14:44:30 -0800

    Test FindModules

--- common_test.go
+++ common_test.go
@@ -15,11 +15,15 @@
 package tools
 
 import (
+	"fmt"
+	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"golang.org/x/mod/modfile"
 )
 
 func TestFindRepoRoot(t *testing.T) {
@@ -29,4 +33,47 @@ func TestFindRepoRoot(t *testing.T) {
 	actual, err := FindRepoRoot()
 	require.NoError(t, err)
 	assert.Equal(t, expected, actual)
+}
+
+func TestFindModules(t *testing.T) {
+	root := t.TempDir()
+	dirs := []string{
+		root,
+		filepath.Join(root, "a"),
+		filepath.Join(root, "a/b"),
+		filepath.Join(root, "c"),
+	}
+	for _, d := range dirs {
+		os.MkdirAll(d, os.ModePerm)
+		goMod := filepath.Join(d, "go.mod")
+		f, err := os.Create(goMod)
+		require.NoError(t, err)
+		modName := strings.Replace(d, root, "fake.multi.mod.project", 1)
+		fmt.Fprintf(f, "module %s\n", modName)
+		require.NoError(t, f.Close())
+	}
+	// Add a non-module dir.
+	os.MkdirAll(filepath.Join(root, "tools"), os.ModePerm)
+
+	got, err := FindModules(root)
+	require.NoError(t, err)
+	require.Len(t, got, len(dirs), "number of found modules")
+	for i, d := range dirs {
+		assert.Equal(t, filepath.Join(d, "go.mod"), got[i].Syntax.Name)
+	}
+}
+
+func TestFindModulesReturnsErrorForInvalidGoModFile(t *testing.T) {
+	root := t.TempDir()
+	goMod := filepath.Join(root, "go.mod")
+	f, err := os.Create(goMod)
+	require.NoError(t, err)
+	fmt.Fprintln(f, "invalid file format")
+	require.NoError(t, f.Close())
+
+	_, err = FindModules(root)
+	require.IsType(t, modfile.ErrorList{}, err)
+	errList := err.(modfile.ErrorList)
+	require.Len(t, errList, 1, "unexpected errors")
+	assert.EqualError(t, errList[0].Err, "unknown directive: invalid")
 }
