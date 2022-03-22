7fe5d4b7f305b472ca987c4b6cc47e59546a9867
bryan-aguilar <46550959+bryan-aguilar@users.noreply.github.com>
2022-03-22 15:55:41 -0700

    Change command config creation logic (#87)
    * Change command config creation logic
    
    * Remove use of new()
    
    Co-authored-by: Anthony Mirabella <a9@aneurysm9.com>
    
    Co-authored-by: Anthony Mirabella <a9@aneurysm9.com>

--- crosslink/cmd/root.go
+++ crosslink/cmd/root.go
@@ -31,7 +31,10 @@ type commandConfig struct {
 	pruneCommand cobra.Command
 }
 
-func (c *commandConfig) buildCommands() {
+func newCommandConfig() *commandConfig {
+	c := &commandConfig{
+		runConfig: cl.DefaultRunConfig(),
+	}
 	preRunSetup := func(cmd *cobra.Command, args []string) {
 		c.runConfig.ExcludedPaths = transformExclude(c.excludeFlags)
 
@@ -80,11 +83,11 @@ func (c *commandConfig) buildCommands() {
 		},
 	}
 	c.rootCommand.AddCommand(&c.pruneCommand)
-
+	return c
 }
 
 var (
-	comCfg = new(commandConfig)
+	comCfg = newCommandConfig()
 )
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -98,7 +101,6 @@ func Execute() {
 }
 
 func init() {
-	comCfg.buildCommands()
 
 	comCfg.rootCommand.PersistentFlags().StringVar(&comCfg.runConfig.RootPath, "root", "", "path to root directory of multi-module repository")
 	comCfg.rootCommand.PersistentFlags().StringSliceVar(&comCfg.excludeFlags, "exclude", []string{}, "list of comma separated go modules that crosslink will ignore in operations."+
