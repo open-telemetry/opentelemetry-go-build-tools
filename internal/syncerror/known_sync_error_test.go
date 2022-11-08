8d298a5d8977165bc2c90b6cfd5268202cceb0c1
Bryan Aguilar <bryaag@amazon.com>
2022-11-07 19:24:25 -0800

    Move build declarations

--- internal/syncerror/known_sync_error_test.go
+++ internal/syncerror/known_sync_error_test.go
@@ -1,6 +1,3 @@
-//go:build !windows
-// +build !windows
-
 // Copyright The OpenTelemetry Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,6 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+//go:build !windows
+// +build !windows
 
 package syncerror
 
