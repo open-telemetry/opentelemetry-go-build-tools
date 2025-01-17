90c9b098dc0eebbe03316c039048d5bc751827ac
Curtis Robert <crobert@splunk.com>
2025-01-17 09:28:02 -0800

    [chore][githubgen] Remove unnecessary trailing whitespace (#663)

--- githubgen/codeowners.go
+++ githubgen/codeowners.go
@@ -141,7 +141,7 @@ LOOP:
 		for stability := range m.Status.Stability {
 			if stability == unmaintainedStatus {
 				unmaintainedList += key + "/\n"
-				unmaintainedCodeowners += fmt.Sprintf("%s/%s @open-telemetry/collector-contrib-approvers \n", key, strings.Repeat(" ", data.MaxLength-len(key)))
+				unmaintainedCodeowners += fmt.Sprintf("%s/%s @open-telemetry/collector-contrib-approvers\n", key, strings.Repeat(" ", data.MaxLength-len(key)))
 				continue LOOP
 			}
 			if stability == "deprecated" && (m.Status.Codeowners == nil || len(m.Status.Codeowners.Active) == 0) {
