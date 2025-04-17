a593218e1ad70f7e13abadd0b13a0aecffac3079
Tyler Yahn <MrAlias@users.noreply.github.com>
2025-04-17 09:31:58 -0700

    Update chloggen/main.go

--- chloggen/main.go
+++ chloggen/main.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Chloggen generates chagnelog entries.
+// Chloggen generates changelog entries.
 package main
 
 import (
