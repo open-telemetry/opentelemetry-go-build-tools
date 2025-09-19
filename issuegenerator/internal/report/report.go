1878e3027a97163f6067b5784dfeadb3ef37155d
Pablo Baeyens <pbaeyens31+github@gmail.com>
2025-09-19 18:23:31 +0200

    [issuegenerator] Refactor and divide into packages

---
+++ issuegenerator/internal/report/report.go
@@ -0,0 +1,39 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package report
+
+import "strings"
+
+type Report struct {
+	Module      string
+	FailedTests map[string]string
+}
+
+// getFailedTests returns information about failed tests if available, otherwise
+// an empty string.
+func (r *Report) FailedTestsMD() string {
+	if len(r.FailedTests) == 0 {
+		return ""
+	}
+
+	var sb strings.Builder
+	sb.WriteString("#### Test Failures\n")
+
+	for testName, testError := range r.FailedTests {
+		sb.WriteString("-  `" + testName + "`\n```\n" + testError + "\n```\n")
+	}
+
+	return sb.String()
+}
