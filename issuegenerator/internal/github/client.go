88212a082ae158ab62736f5471097905c35556e1
Pablo Baeyens <pbaeyens31+github@gmail.com>
2025-09-19 18:36:11 +0200

    Package:

--- issuegenerator/internal/github/client.go
+++ issuegenerator/internal/github/client.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// package github handles interaction with Github through the [Client].
+// Package github handles interaction with Github through the [Client].
 package github
 
 import (
