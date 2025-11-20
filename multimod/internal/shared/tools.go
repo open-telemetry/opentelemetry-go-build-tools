e28bccbfbc59d6d7a1ae713f457069b5ad630bd0
dmathieu <42@dmathieu.com>
2025-11-20 13:59:16 +0100

    ignore revive lint issue

--- multimod/internal/shared/tools.go
+++ multimod/internal/shared/tools.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package shared
+package shared // nolint:revive // keeping generic package name until a proper refactoring is done
 
 import (
 	"errors"
