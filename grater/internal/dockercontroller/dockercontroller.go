6167ba3f07cf0a5bcab0e5a4c6e6e473c7023d78
Amisha Chhajed <amishhhaaaa@gmail.com>
2026-04-16 21:03:03 +0530

    fix: add integration tag in dockercontroller to skip in codecov
    Signed-off-by: Amisha Chhajed <amishhhaaaa@gmail.com>

--- grater/internal/dockercontroller/dockercontroller.go
+++ grater/internal/dockercontroller/dockercontroller.go
@@ -1,6 +1,9 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
+//go:build integration
+// +build integration
+
 // Package dockercontroller provides a controller for managing Docker.
 package dockercontroller
 
