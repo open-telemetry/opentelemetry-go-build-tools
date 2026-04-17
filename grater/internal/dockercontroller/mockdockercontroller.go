f7cfe043929c0bd0e808232dc2afddab10e37503
Amisha Chhajed <amishhhaaaa@gmail.com>
2026-04-17 20:07:41 +0530

    fix: vul warnings by adding build tag

--- grater/internal/dockercontroller/mockdockercontroller.go
+++ grater/internal/dockercontroller/mockdockercontroller.go
@@ -1,6 +1,9 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
+//go:build integration
+// +build integration
+
 // Package dockercontroller provides a mock implementation of the DockerController interface.
 package dockercontroller
 
