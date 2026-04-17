229b2a6ab3518b64059096ee817025b441a25b87
Amisha Chhajed <amishhhaaaa@gmail.com>
2026-04-17 20:24:02 +0530

    fix: vul warnings by adding build tag

--- grater/internal/container/container.go
+++ grater/internal/container/container.go
@@ -1,6 +1,9 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
+//go:build integration
+// +build integration
+
 // Package container provides an interface for managing Docker containers and volumes.
 package container
 
