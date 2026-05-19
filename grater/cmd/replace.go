3c456bf52d8bca469aff8994bc6c330880444e9d
Amisha Chhajed <amishhhaaaa@gmail.com>
2026-05-19 22:02:26 +0530

    replace: add command replace
    Signed-off-by: Amisha Chhajed <amishhhaaaa@gmail.com>

--- grater/cmd/replace.go
+++ grater/cmd/replace.go
@@ -2,3 +2,55 @@
 // SPDX-License-Identifier: Apache-2.0
 
 package cmd
+
+import (
+	"github.com/spf13/cobra"
+
+	"go.opentelemetry.io/build-tools/grater/internal/replacehelper"
+	"go.opentelemetry.io/build-tools/grater/internal/workspace"
+)
+
+var replacePath string
+
+// replaceCmd represents the replace command
+func replaceCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "replace [replacements...]",
+		Short: "Adds one or more replacements to be tested.",
+		Long: "Adds one or more replacements to be tested. " +
+			"The replacements can be specified as command line arguments or in a .txt file, or both.",
+		Example: `
+grater replace github.com/foo/bar github.com/foo/bar@v1.0.0
+grater replace github.com/foo/bar@v1.0.0 ../local/module
+grater replace --file replacements.txt
+grater replace -f replacements.txt
+`,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			ws, err := workspace.NewWorkspace()
+			if err != nil {
+				return err
+			}
+
+			if replacePath != "" {
+				if err = replacehelper.AddFromFile(ws, replacePath); err != nil {
+					return err
+				}
+			}
+
+			if len(args) > 0 {
+				if err = replacehelper.Replace(ws, []string{
+					args[0] + " " + args[1],
+				}); err != nil {
+					return err
+				}
+			}
+
+			cmd.Printf("Successfully added replacements.\n")
+			return nil
+		},
+	}
+
+	cmd.Flags().StringVarP(&replacePath, "file", "f", "", "path to the replacements file")
+
+	return cmd
+}
--- grater/cmd/replace_test.go
+++ grater/cmd/replace_test.go
@@ -2,3 +2,74 @@
 // SPDX-License-Identifier: Apache-2.0
 
 package cmd
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+const replaceUsage = `Adds one or more replacements to be tested. The replacements can be specified as command line arguments or in a .txt file, or both.
+
+Usage:
+  grater replace [replacements...] [flags]
+
+Examples:
+
+grater replace github.com/foo/bar github.com/foo/bar@v1.0.0
+grater replace github.com/foo/bar@v1.0.0 ../local/module
+grater replace --file replacements.txt
+grater replace -f replacements.txt
+
+
+Flags:
+  -f, --file string   path to the replacements file
+  -h, --help          help for replace`
+
+func TestReplace(t *testing.T) {
+	out, err := runCobra(t, "replace", "--help")
+	require.NoError(t, err)
+
+	assert.Contains(t, out, replaceUsage)
+}
+
+func TestReplaceCmd(t *testing.T) {
+	testDir := t.TempDir()
+	t.Chdir(testDir)
+
+	out, err := runCobra(
+		t,
+		"replace",
+		"foo/bar@v1.0.0",
+		"baz/qux@v2.0.0",
+	)
+	require.NoError(t, err)
+
+	assert.Contains(t, out, "Successfully added replacements.")
+
+	err = os.WriteFile(
+		"replacements.txt",
+		[]byte("foo/bar baz/qux@v1.0.0\nabc/def@v2.0.0 xyz/pqr\n"),
+		fileReadWrite,
+	)
+	require.NoError(t, err)
+
+	out, err = runCobra(t, "replace", "--file", "replacements.txt")
+	require.NoError(t, err)
+
+	assert.Contains(t, out, "Successfully added replacements.")
+
+	out, err = runCobra(
+		t,
+		"replace",
+		"foo/bar",
+		"baz/qux",
+		"--file",
+		"replacements.txt",
+	)
+	require.NoError(t, err)
+
+	assert.Contains(t, out, "Successfully added replacements.")
+}
