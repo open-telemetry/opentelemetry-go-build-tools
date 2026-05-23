c52fe8a77913d09c54ef64310af1aaa5c0c3d261
Amisha Chhajed <amishhhaaaa@gmail.com>
2026-05-24 00:15:35 +0530

    report: Add methods to generate report from execute commands responses.
    Signed-off-by: Amisha Chhajed <amishhhaaaa@gmail.com>

---
+++ grater/internal/report/report_test.go
@@ -0,0 +1,58 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+package report
+
+import (
+	"context"
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/build-tools/grater/internal/container"
+	"go.opentelemetry.io/build-tools/grater/internal/module"
+)
+
+func TestClassifyResult(t *testing.T) {
+	assert.Equal(t, "pass", classifyResult(container.ExecuteCommandResponse{ExitCode: 0}, container.ExecuteCommandResponse{ExitCode: 0}))
+	assert.Equal(t, "broken", classifyResult(container.ExecuteCommandResponse{ExitCode: 1}, container.ExecuteCommandResponse{ExitCode: 1}))
+	assert.Equal(t, "regression", classifyResult(container.ExecuteCommandResponse{ExitCode: 0}, container.ExecuteCommandResponse{ExitCode: 1}))
+	assert.Equal(t, "broken", classifyResult(container.ExecuteCommandResponse{ExitCode: 1}, container.ExecuteCommandResponse{ExitCode: 0}))
+}
+
+func TestGetReport(t *testing.T) {
+	bytes, err := GetReport(context.Background(),
+		[]module.Module{*module.NewModule("moduleA", ""), *module.NewModule("moduleB", ""), *module.NewModule("moduleC", "")},
+		[][]container.ExecuteCommandResponse{
+			{{ExitCode: 0, Output: "ok"}, {ExitCode: 0, Output: "ok"}},
+			{{ExitCode: 1, Output: "fail"}, {ExitCode: 1, Output: "fail"}},
+			{{ExitCode: 0, Output: "ok"}, {ExitCode: 1, Output: "fail"}},
+		},
+	)
+	require.NoError(t, err)
+
+	var report []Result
+	require.NoError(t, json.Unmarshal(bytes, &report))
+
+	assert.Equal(t, Result{Dependent: "moduleA", Status: "pass", BaseOutput: "ok", HeadOutput: "ok"}, report[0])
+	assert.Equal(t, Result{Dependent: "moduleB", Status: "broken", BaseOutput: "fail", HeadOutput: "fail"}, report[1])
+	assert.Equal(t, Result{Dependent: "moduleC", Status: "regression", BaseOutput: "ok", HeadOutput: "fail"}, report[2])
+}
+
+func TestGetRegressionReport(t *testing.T) {
+	bytes, err := GetRegressionReport(context.Background(),
+		[]module.Module{*module.NewModule("moduleA", ""), *module.NewModule("moduleB", ""), *module.NewModule("moduleC", "")},
+		[][]container.ExecuteCommandResponse{
+			{{ExitCode: 0, Output: "ok"}, {ExitCode: 0, Output: "ok"}},
+			{{ExitCode: 1, Output: "fail"}, {ExitCode: 1, Output: "fail"}},
+			{{ExitCode: 0, Output: "ok"}, {ExitCode: 1, Output: "fail"}},
+		},
+	)
+	require.NoError(t, err)
+
+	var report []Result
+	require.NoError(t, json.Unmarshal(bytes, &report))
+
+	assert.Len(t, report, 1)
+	assert.Equal(t, Result{Dependent: "moduleC", Status: "regression", BaseOutput: "ok", HeadOutput: "fail"}, report[0])
+}
